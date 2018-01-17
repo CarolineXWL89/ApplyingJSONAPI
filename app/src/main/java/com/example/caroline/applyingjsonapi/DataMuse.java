bbcfe3bb37e1124c0b0b4365646c94f1ef5b2b4b
CarolineXWL89 <carolinexwliu89@hotmail.com>
2018-01-17 14:00:43 -0800

    #1 - Need to revise

---
+++ app/src/main/java/com/example/caroline/applyingjsonapi/DataMuse.java
@@ -0,0 +1,11 @@
+package com.example.caroline.applyingjsonapi;
+
+/**
+ * Created by princ on 17/01/2018.
+ */
+
+public interface DataMuse {
+
+    String baseURL = "https://www.goodreads.com/book/";
+
+}
