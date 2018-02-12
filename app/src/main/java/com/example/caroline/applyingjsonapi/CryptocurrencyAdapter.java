51212080db629796376d217ed22b283988dfbb64
CarolineXWL89 <carolinexwliu89@hotmail.com>
2018-02-12 14:01:31 -0800

    #5 started setting up cardview

---
+++ app/src/main/java/com/example/caroline/applyingjsonapi/CryptocurrencyAdapter.java
@@ -0,0 +1,12 @@
+package com.example.caroline.applyingjsonapi;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by princ on 12/02/2018.
+ */
+
+public class CryptocurrencyAdapter extends RecyclerView.Adapter<CryptocurrencyAdapter.MyViewHolder> {
+
+
+}
--- app/src/main/java/com/example/caroline/applyingjsonapi/MainActivity.java
+++ app/src/main/java/com/example/caroline/applyingjsonapi/MainActivity.java
@@ -2,14 +2,22 @@ package com.example.caroline.applyingjsonapi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private CryptocurrencyAdapter cryptocurrencyAdapter;
+    private List<Cryptocurrency> cryptocurrencies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,18 @@ public class MainActivity extends AppCompatActivity {
         DataMuse api = retrofit.create(DataMuse.class);
 
         Call<List<Cryptocurrency>> call = api.getNumInfosRanked(10);
+
+        call.enqueue(new Callback<List<Cryptocurrency>>() {
+            @Override
+            public void onResponse(Call<List<Cryptocurrency>> call, Response<List<Cryptocurrency>> response) {
+                cryptocurrencies.clear();
+            }
+
+            @Override
+            public void onFailure(Call<List<Cryptocurrency>> call, Throwable t) {
+
+            }
+        });
 
         /*
         Retrofit retrofit = new Retrofit.Builder()
